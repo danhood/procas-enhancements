a8eb949fd2276b4ee5abc404a2358dd4581d7bde
Ilya Dynin <idynin@gmail.com>
2016-03-01 19:07:42 -0500

    Bump version for auto-update

--- procas-enhancements.user.js
+++ procas-enhancements.user.js
@@ -6,7 +6,7 @@
 // @match         https://www.procastime.com/time/*/Modules/TimeSystem/TimeCard/mytimecard.aspx*
 // @match         https://www.procastime.com/time/*/Modules/TimeSystem/TimeCard/View*.aspx*
 // @match         https://www.procastime.com/time/*/modules/TimeSystem/TimeCard/View*.aspx*
-// @version       0.3
+// @version       0.3.1
 // ==/UserScript==
 
 (function() {
